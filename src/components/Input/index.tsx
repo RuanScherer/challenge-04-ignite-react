dfe0d787783cb2b2b4262c43b5e4ecd1c4003dee
Ruan Scherer <ruan.scherer@outlook.com>
2022-02-24 18:52:26 -0300

    use typescript and function components

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import {
+import React, {
   useEffect,
   useRef,
   useState,
@@ -9,8 +9,14 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string
+  placeholder?: string
+  icon?: React.FC<{ size: number }>
+}
+
+export const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +55,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Food {
+  id?: number
+  image: string
+  name: string
+  price: number
+  description: string
+  available: boolean
+}
